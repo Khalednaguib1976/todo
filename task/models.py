14c03ae29de4f3b3c68a010df2e98f79033a6a0e
Your Name <you@example.com>
2020-10-09 04:30:44 -0700

    TodoWith_DB_Proplem

---
+++ task/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+
+class task (models.Model):
+    title = models.CharField(max_length=50)
+    asign_date = models.DateTimeField(default=timezone.now)
+    Subject = models.TextField(max_length=1000)
+    Target_Date = models.DateTimeField
+    Done = models.BooleanField
+    Actualdate = models.DateTimeField
+    Doc =models.FileField
+    image =models.ImageField (upload_to='tasksImage/')
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta :
+        ordering=('-id',)
+        verbose_name='Task'
+        verbose_name_plural='Tasks'
+
+
+
+# Create your models here.
